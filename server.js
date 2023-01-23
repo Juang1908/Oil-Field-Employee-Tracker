010009f3faa2acea062bda7e32bf2158e9582557
Mr. Galvan <112872430+Juang1908@users.noreply.github.com>
2023-01-23 12:50:14 -0600

    added ctable

--- server.js
+++ server.js
@@ -1,2 +1,17 @@
-const inquirer = require('inquirer');
+// Packages Required
+const inquirer = require('inquirer'); 
 const mysql = require('mysql2');
+const table = require('console.table');
+
+
+const db = mysql.createConnection(
+    {
+      host: 'localhost',
+      // MySQL username,
+      user: 'root',
+      // TODO: Add MySQL password
+      password: '',
+      database: 'employee_db'
+    },
+    console.log(`Connected to the employee_db database.`)
+  );
