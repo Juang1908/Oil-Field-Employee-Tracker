66caed5c8a0ba272683c99d342644d50d9d07a96
Mr. Galvan <112872430+Juang1908@users.noreply.github.com>
2023-01-23 12:27:18 -0600

    gitignore

--- db/schema.sql
+++ db/schema.sql
@@ -3,15 +3,30 @@ CREATE DATABASE employee_db;
 
 USE employee_db;
 
-ADD TABLE employee (
+DROP TABLE IF EXISTS departmentChart;
+DROP TABLE IF EXISTS roleChart;
+DROP TABLE IF EXISTS employeeChart;
 
-
+CREATE TABLE departmentChart (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    deptName VARCHAR(50) NOT NULL, 
 );
 
-ADD TABLE role (
-
+CREATE TABLE roleChart (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(50) NOT NULL,
+    salary  DECIMAL NOT NULL,
+    deptId INT,
+    FOREIGN KEY (deptId) REFERENCES departmentChart(id) ON DELETE SET NULL
 );
 
-ADD TABLE department (
-
+CREATE TABLE employeeChart (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    firstName VARCHAR(50) NOT NULL,
+    lastName VARCHAR(50) NOT NULL,
+    roleId INT,
+    managerId INT
+    FOREIGN KEY (roleId) REFERENCES employeeChart(id) ON DELETE SET NULL,
+    FOREIGN KEY (managerId) REFERENCES roleChart(id) ON DELETE SET NULL,
 );
+
